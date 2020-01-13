f6fce1c40e172072d1653b6a8173bede6b8ac23a
zhangruiling <824703666@qq.com>
2020-01-13 17:04:59 +0800

    自动轮播

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@
     var $bannerli = $('.banner-content ul li');
     var $btnli = $('.nav-tab ul li');
     var num = 0; 
+    var timer=null;
+    var $piclilength = $bannerli.size();
     $btnli.on('click', function() {
         num = $(this).index();
         tabswitch();
@@ -12,4 +14,23 @@
         $btnli.eq(num).addClass('tabActive').siblings().removeClass('tabActive');
         $bannerli.eq(num).animate({ opacity: 1 }).siblings($bannerli).animate({ opacity: 0 });
     }
+    timer = setInterval(function() {
+        num++;
+        if (num > $piclilength - 1) {
+            num = 0;
+        }
+        tabswitch();
+    }, 6000);
+
+    $banner.hover(function() {
+        clearInterval(timer);
+    }, function() {
+        timer = setInterval(function() {
+            num++;
+            if (num > $piclilength - 1) {
+                num = 0;
+            }
+            tabswitch();
+        }, 6000);
+    });
 })(jQuery);
